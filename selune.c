f32317e7f116baa479fea1c6db55f969fd9abf3b
cat <arcnyxx@proton.me>
2022-11-17 21:17:21 -0600

    move input to function, ready for output, update man page

--- selune.c
+++ selune.c
@@ -28,6 +28,8 @@
 
 static void die(const char *fmt, ...);
 static void *srealloc(void *ptr, size_t len);
+static char *getsel(xcb_window_t win, xcb_atom_t sel, xcb_atom_t trg,
+		xcb_atom_t incr, size_t *len);
 
 xcb_connection_t *conn;
 
@@ -54,10 +56,52 @@ srealloc(void *ptr, size_t len)
 	return ptr;
 }
 
+static char *
+getsel(xcb_window_t win, xcb_atom_t sel, xcb_atom_t trg,
+		xcb_atom_t incr, size_t *len)
+{
+	xcb_generic_event_t *evt;
+	GA(plac, "PLAC", 4) CA(plac, "PLAC")
+	xcb_convert_selection(conn, win, sel, trg, plac, XCB_CURRENT_TIME);
+	xcb_flush(conn);
+	while ((evt = xcb_wait_for_event(conn)) != NULL &&
+			(evt->response_type & ~0x80) != XCB_SELECTION_NOTIFY)
+		free(evt);
+	if (((xcb_selection_notify_event_t *)evt)->property == 0)
+		die("selune: unable to convert selection: %s\n", trg);
+
+	char *buf = NULL;
+	if (((xcb_selection_notify_event_t *)evt)->target != incr) {
+		GP(prop, plac, "PLAC")
+		buf = srealloc(buf, *len += prop.name_len);
+		memcpy(buf, prop.name, prop.name_len);
+		xcb_icccm_get_text_property_reply_wipe(&prop);
+		return buf;
+	}
+
+run:
+	xcb_delete_property(conn, win, plac);
+	while ((evt = xcb_wait_for_event(conn)) != NULL &&
+			(evt->response_type & ~0x80) != XCB_PROPERTY_NOTIFY &&
+			((xcb_property_notify_event_t *)evt)->state != 
+			XCB_PROPERTY_NEW_VALUE)
+		free(evt);
+
+	GP(prop, plac, "PLAC")
+	if (prop.name_len != 0) {
+		buf = srealloc(buf, *len += prop.name_len);
+		memcpy(buf, prop.name, prop.name_len);
+		xcb_icccm_get_text_property_reply_wipe(&prop);
+		goto run;
+	}
+	xcb_icccm_get_text_property_reply_wipe(&prop);
+	return buf;
+}
+
 int
 main(int argc, char **argv)
 {
-	const char *sel = "CLIPBOARD", *trg = "UTF8_STRING";
+	const char *sel = "CLIPBOARD", *trg = "TEXT";
 	if (*(argv = &argv[1]) != NULL && (*argv)[0] == '-') {
 		bool end = false; char *str = argv[0];
 		for (int i = 1; str[i] != '\0'; ++i)
@@ -94,45 +138,26 @@ main(int argc, char **argv)
 			XCB_EVENT_MASK_PROPERTY_CHANGE })) != NULL)
 		die("selune: unable to create window\n");
 
-	GA(asel, sel, strlen(sel)) GA(atrg, trg, strlen(trg))
-	GA(incr, "INCR", 4) GA(plac, "PLAC", 4)
-	CA(asel, sel) CA(atrg, trg) CA(incr, "INCR") CA(plac, "PLAC")
+	GA(asel, sel, strlen(sel)) GA(atrg, trg, strlen(trg)) GA(incr, "INCR",
+	4) GA(targ, "TARGETS", 7) GA(mult, "MULTIPLE", 8) CA(asel, sel)
+	CA(atrg, trg) CA(incr, "INCR") CA(targ, "TARGETS") CA(mult, "MULTIPLE") 
 
-	xcb_generic_event_t *evt;
-	xcb_convert_selection(conn, win, asel, atrg, plac, XCB_CURRENT_TIME);
-	xcb_flush(conn);
-	while ((evt = xcb_wait_for_event(conn)) != NULL &&
-			(evt->response_type & ~0x80) != XCB_SELECTION_NOTIFY)
-		free(evt);
-	if (((xcb_selection_notify_event_t *)evt)->property == 0)
-		die("selune: unable to convert selection: %s\n", trg);
-
-	char *buf = NULL; size_t len = 0;
-	if (((xcb_selection_notify_event_t *)evt)->target != incr) {
-		GP(prop, plac, "PLAC")
-		buf = srealloc(buf, len += prop.name_len);
-		memcpy(buf, prop.name, prop.name_len);
-		xcb_icccm_get_text_property_reply_wipe(&prop);
-		goto esc;
+	size_t len = 0; char *buf = NULL;
+	if (isatty(STDIN_FILENO)) {
+		buf = getsel(win, asel, atrg, incr, &len);
+	} else {
+		long ret, tot;
+		buf = srealloc(buf, tot = 256);
+		while ((ret = read(STDIN_FILENO, &buf[len],
+				tot - len)) == (long)(tot - len))
+			buf = srealloc(buf, tot *= 2), len += ret;
+		if (ret == -1)
+			die("selune: unable to read from stdin: ");
+		buf = srealloc(buf, len += ret);
 	}
+	if (len != 0)         write(STDOUT_FILENO, buf, len);
+	if (fork() != 0)      return 0;
+	if (chdir("/") == -1) die("selune: unable to chdir: /: ");
 
-run:
-	xcb_delete_property(conn, win, plac);
-	while ((evt = xcb_wait_for_event(conn)) != NULL &&
-			(evt->response_type & ~0x80) != XCB_PROPERTY_NOTIFY &&
-			((xcb_property_notify_event_t *)evt)->state != 
-			XCB_PROPERTY_NEW_VALUE)
-		free(evt);
-
-	GP(prop, plac, "PLAC")
-	if (prop.name_len != 0) {
-		buf = srealloc(buf, len += prop.name_len);
-		memcpy(buf, prop.name, prop.name_len);
-		xcb_icccm_get_text_property_reply_wipe(&prop);
-		goto run;
-	}
-	xcb_icccm_get_text_property_reply_wipe(&prop);
-
-esc:
-	write(STDOUT_FILENO, buf, len);
+	xcb_disconnect(conn);
 }
